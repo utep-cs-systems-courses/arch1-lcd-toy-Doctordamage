a197ec37233af4fbe48ba9be863fdcdd2417fad6
student <daayala2@miners.utep.edu.com>
2021-05-08 05:30:57 -0600

    modified led.c

--- project/led.c
+++ project/led.c
@@ -1,48 +1,24 @@
 #include <msp430.h>
 #include "led.h"
-#include "switches.h"
-#include "buzzer.h"
+//#include "switches.h"
 
-unsigned char led_changed = 0, green_on=0, red_on=0, led_state=0;
-char ledFlags = 0;
+unsigned char red_on = 0, green_on = 0;
+unsigned char led_changed = 0;
 
-// Keep P1 because LEDs are on the board
-void led_init()
+static char redVal[] = {0, RED_LED}, greenVal[] = {0, GREEN_LED};
 
-{
-  P1DIR |= LEDS;                      // bits attached to leds are output
+void led_init() {
+  P1DIR |= LEDS;// bits attached to leds are output
   led_changed = 1;
   led_update();
 }
 
-void led_update()
-{
-  if (led_changed)
-    {
-      ledFlags=0;
-      if(!(led_state)){
-	ledFlags |= LED_RED;
-      }
+void led_update() {
+  if (led_changed) {
+    char ledFlags = redVal[red_on] | greenVal[green_on];
 
-      else
-	{
-	  ledFlags |= LED_GREEN;
-	}
-
-      P1OUT &= (0xff^LEDS) | ledFlags; // clear bit for off leds
-      P1OUT |= ledFlags;     // set bit for on leds
-      led_changed = 0;
-    }
-}
-
-void led_toggle()
-{
-  led_state = 1 - led_state;
-  led_changed = 1;
-}
-
-void led_advance()
-{
-  led_state+=1;
-  led_changed = 1;
+    P1OUT &= (0xff^LEDS) | ledFlags; // clear bit for off leds
+    P1OUT |= ledFlags;     // set bit for on leds
+    led_changed = 0;
+  }
 }
